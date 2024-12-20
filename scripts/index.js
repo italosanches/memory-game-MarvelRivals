556ddd65f6fd4d32df41aa4406d22a59bf230610
italosanches <italo_carvalho157@hotmail.com>
2024-12-20 17:09:27 -0300

    adicionado armazenamento na session storage

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+function getOptionsAndUser() {
+	let optionsAndUser = {};
+	optionsAndUser["user"] = document.querySelector("#user").value;
+	optionsAndUser["cardsQuantity"] = document.querySelector("input[type=radio]:checked").value;
+	return optionsAndUser;
+}
+function setSessionStorageInGameStart(e) {
+	e.preventDefault();
+	const usernameAndCardQuantity = getOptionsAndUser();
+	sessionStorage.setItem("userAndCardsQuantity", JSON.stringify(usernameAndCardQuantity));
+	window.location.href = "../game/game.html";
+}
