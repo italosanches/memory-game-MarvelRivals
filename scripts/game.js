a405699331026fb85b3f47b922104ce8b484f653
italosanches <italo_carvalho157@hotmail.com>
2024-12-23 17:57:56 -0300

    ajustes

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,15 @@
-import createArrayOfRandomCards from "./cards.js";
-document.addEventListener("DOMContentLoaded", getUserAndOptionsFromSessionStorage);
+// import createArrayOfRandomCards from "./cards.js";
+// document.addEventListener("DOMContentLoaded", getUserAndOptionsFromSessionStorage);
 
-async function getUserAndOptionsFromSessionStorage() {
-	const optionsAndUser = JSON.parse(window.sessionStorage.getItem("userAndCardsQuantity"));
-	const cards = createArrayOfRandomCards(optionsAndUser.cardsQuantity);
-	console.log(cards);
+// async function getUserAndOptionsFromSessionStorage() {
+// 	const optionsAndUser = JSON.parse(window.sessionStorage.getItem("userAndCardsQuantity"));
+// 	const cards = createArrayOfRandomCards(optionsAndUser.cardsQuantity);
+// 	console.log(cards);
+// }
+
+const card = document.querySelectorAll(".card-game");
+card.forEach((e) => e.addEventListener("click", flipCard));
+
+function flipCard() {
+	this.classList.toggle("flipCard"); //card clicado
 }
