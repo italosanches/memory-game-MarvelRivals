abb971f47bdb4959ef1e1f6c3ca52463758bb108
italosanches <italo_carvalho157@hotmail.com>
2024-12-26 17:59:42 -0300

    Novas funcionalidades, e ajuste de layout.

--- scripts/game.js
+++ scripts/game.js
@@ -1,15 +1,46 @@
-// import createArrayOfRandomCards from "./cards.js";
-// document.addEventListener("DOMContentLoaded", getUserAndOptionsFromSessionStorage);
+import createArrayOfRandomCards from "./cards.js";
+document.addEventListener("DOMContentLoaded", getUserAndOptionsFromSessionStorage);
 
-// async function getUserAndOptionsFromSessionStorage() {
-// 	const optionsAndUser = JSON.parse(window.sessionStorage.getItem("userAndCardsQuantity"));
-// 	const cards = createArrayOfRandomCards(optionsAndUser.cardsQuantity);
-// 	console.log(cards);
-// }
+document.querySelector("#btn-start-game").addEventListener("click", startGame);
+
+async function getUserAndOptionsFromSessionStorage() {
+	const optionsAndUser = JSON.parse(window.sessionStorage.getItem("userAndCardsQuantity"));
+	return optionsAndUser;
+}
 
 const card = document.querySelectorAll(".card-game");
 card.forEach((e) => e.addEventListener("click", flipCard));
 
 function flipCard() {
 	this.classList.toggle("flipCard"); //card clicado
+}
+
+function createCard(card) {
+	const cardElement = document.createElement("div");
+	cardElement.classList.add("card-game");
+
+	const cardFront = document.createElement("div");
+	cardFront.classList.add("card-front");
+
+	const cardBack = document.createElement("div");
+	cardFront.classList.add("card-back");
+
+	cardElement.appendChild(cardFront);
+	cardElement.appendChild(cardBack);
+	cardElement.addEventListener("click", flipCard);
+	return cardElement;
+}
+
+function renderCards(cards) {
+	const boardGame = (document.querySelector(".board-game").innerHTML = "");
+	cards.forEach((card) => {
+		const cardElement = createCard(card);
+		boardGame.appendChild(cardElement);
+	});
+}
+
+async function startGame() {
+	const user = await getUserAndOptionsFromSessionStorage();
+	const cards = createArrayOfRandomCards(16);
+	renderCards(cards);
 }
